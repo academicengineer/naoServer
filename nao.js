b10dfa680c10ab1868a23dd2e31fef4fd0d73a05
Toshiyuki SHIMAZAKI, Academic Eniginner, PhD Student @UEC <79896705+academicengineer@users.noreply.github.com>
2021-08-06 11:41:00 +0900

    Add files via upload

---
+++ nao.js
@@ -0,0 +1,36 @@
+$(function(){
+    var qis, ip, als = {};
+
+    // 接続ボタンclickイベント
+    $('#connect-btn').on('click', function(){
+        // 入力IP取得
+        ip = $('#ip').val();
+        // NAOqi Session 生成
+        qis = new QiSession(ip);
+        // 接続
+        qis.socket()
+        .on('connect', function(){
+            // 接続成功
+            console.log('[CONNECTED]');
+            // ALTextToSpeechを使う
+            qis.service('ALTextToSpeech').done(function(ins){
+                als.alTextToSpeech = ins;
+            });
+        })
+        .on('disconnect', function(){
+            // 接続断
+            console.log('[DISCONNECTED]');
+        })
+        .on('error', function(){
+            // 接続エラー
+            console.log('[CONNECTION ERROR]');
+        });
+    });
+
+    // テストボタンclickイベント
+    $('#test-btn').on('click', function(){
+        // Pepperにしゃべらせる
+        console.log('[TEST]');
+        if(als.alTextToSpeech) als.alTextToSpeech.say('こんにちは、僕はペッパー！');
+    });
+});
