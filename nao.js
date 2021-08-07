b3532a5a3ec61145454f2223bf469722026c29da
academicengineer <sa002394@gl.cc.uec.ac.jp>
2021-08-07 11:36:57 +0900

    講義スライド遷移

--- nao.js
+++ nao.js
@@ -44,10 +44,17 @@ $(function(){
 
         //講義スライドに画面遷移
         window.open('lec01.png', '_blank');
-
         qis.service('ALTextToSpeech').done(function(tts){
             als.alTextToSpeech = tts;
-            tts.say('いかがでしょうか。質問はありますか');
+            tts.say('学習工学は，人間の学びをモデルとしてデザインし，デザインに基づく学習支援システムの開発と評価を行う研究分野です');
+            tts.say('人間の学びとは？という問いを知識工学や認知科学の理論を応用した学術研究分野です');
+        });
+
+        //講義スライドに画面遷移
+        window.open('lec02.png', '_blank');
+        qis.service('ALTextToSpeech').done(function(tts){
+            als.alTextToSpeech = tts;
+            tts.say('いかがでしょうか。質問はありますか。特に柏原研究室では「学習モデル作りこそeLearning研究の本質」と捉え、先進的なeLearningの実現を目指して研究を進めています。');
             tts.say('今日は，学習工学特論の第一回目の講義でした。これで講義を終わります');
             tts.say('ありがとうございました。');
         });
